0bd4821269340b47c46477bfb7178fe1102c65ae
ALEEMUDDIN138 <aleemuddin138@gmail.com>
2026-02-24 16:40:25 +0530

    Define ALB outputs in variables.tf
    Add outputs for ALB including DNS name, target group ARN, listener ARN, and security group ID.

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,3 +1,27 @@
+###############################################
+# ============ ALB OUTPUTS ====================
+###############################################
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "target_group_arn" {
+  description = "ARN of the ALB target group"
+  value       = aws_lb_target_group.app_tg.arn
+}
+
+output "listener_arn" {
+  description = "ARN of the ALB listener"
+  value       = aws_lb_listener.main.arn
+}
+
+output "alb_security_group_id" {
+  description = "Security Group ID of the ALB"
+  value       = aws_security_group.alb_sg.id
+}
+[root@ip-172-31-25-176 alb]# cat variables.tf
 variable "project_name" {
   description = "Project name for tagging resources"
   type        = string
